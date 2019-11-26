c97f8992bda18dc121b967bcaf4306a751956d8a
passthinking <983149482@qq.com>
2019-11-26 08:42:15 +0800

    UPDTA

--- main/main.c
+++ main/main.c
@@ -11,8 +11,6 @@ unsigned char xdata circularQueueBuff[CircularQueueLength];
 void main()
 {
 	unsigned char Number = 0;
-	unsigned char count3 = 0;
-	unsigned char RGBBuff[3];
 
 	unsigned char ch_value = 0;
 	
@@ -28,52 +26,18 @@ void main()
 		Line_1[ Number ] = 0xff;
 		Line_2[ Number ] = 0xff;
 	}
-	
-	setRGB( Line_1 , 0, 0, 0  , 0   );
-	setRGB( Line_2 , 1, 0  , 0, 0   );
-	
 	Uaurt1_Send_String("start up ...");
 	
 	while(1)
 	{
 		waterfall_light( Line_1, Line_2, 3 * 16 );
 		delay50us(5);									//@27.000MHz
-		
 		length = get_DataLength();
 		for( i = 0; i < length; i ++ )
 		{
 			if( get_Data_CircularQueue( &ch_value ) > 0 )
 			{
-			
-				RGBBuff[count3] = ch_value;
-				count3 ++;
-				if(count3 >= 3)
-				{
-					count3 = 0;
-				}
 				
-				for(Number = 0; Number < 16; Number ++)
-				{
-					setRGB( Line_1 , Number, RGBBuff[0], RGBBuff[1], RGBBuff[2] );
-					setRGB( Line_2 , Number, RGBBuff[0], RGBBuff[1], RGBBuff[2] );
-				} 
-				
-//				if(ch_value == "0")
-//				{
-//					for(Number = 0; Number < 3*16; Number ++)
-//					{
-//						Line_1[ Number ] = 0xff;
-//						Line_2[ Number ] = 0xff;
-//					}
-//				}
-//				else
-//				{
-//					for(Number = 0; Number < 3*16; Number ++)
-//					{
-//						Line_1[ Number ] = 0x00;
-//						Line_2[ Number ] = 0x00;
-//					}
-//				}
 				UART1_Send( ch_value );
 			}
 		}
